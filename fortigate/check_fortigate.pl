c38531f7b453ea52104a5759f564f8efedcb529e
sgruber <12892285+sgruber94@users.noreply.github.com>
2021-06-01 13:18:04 +0200

    changed license error message

--- fortigate/check_fortigate.pl
+++ fortigate/check_fortigate.pl
@@ -927,22 +927,26 @@ sub get_license_contract {
    my $k;
    my $lic_contract_cnt = get_snmp_value($session, $oid_license_contract_count );
    if ( $lic_contract_cnt > 0 ) {
+      use constant {
+      DAY => 86400,
+      };
       my %license_contract_descpriction_table = %{get_snmp_table($session, $oid_license_contract_description_table)};
       my %license_contract_expiry_table = %{get_snmp_table($session, $oid_license_contract_expiry_table)};
       my @license_expiry_warn_table;
       my @license_expiry_crit_table;
       $return_state = "OK";
-      $return_string = "All licenses are in appropriate state";
+      #$return_string = "All licenses are in appropriate state";
       $k = 1;
       my $license_actualdate = time();
-      my $license_warning = $warn * 86400;
-      my $license_critcal = $crit * 86400;
+      my $license_warning = $warn * DAY;
+      my $license_critcal = $crit * DAY;
+      my $return_string_errors = "";
       while ($k <= $lic_contract_cnt) {
          my $license_contract_descpriction = $license_contract_descpriction_table{$oid_license_contract_description_table.'.'.$k};
          my $license_contract_expiry = $license_contract_expiry_table{$oid_license_contract_expiry_table.'.'.$k};
          #$license_contract_expiry = str2time($license_contract_expiry); #Parse SNMP String to output
          my $license_remaining = str2time($license_contract_expiry) - $license_actualdate;
-         my $license_remaining_days = $license_remaining / 86400;
+         my $license_remaining_days = $license_remaining / DAY;
         if (($license_remaining <= $license_warning) && ($license_remaining <= $license_critcal) ) {
                 push (@license_expiry_warn_table, ($license_contract_descpriction.'/'.$license_contract_expiry));
               }
@@ -951,14 +955,16 @@ sub get_license_contract {
               }
          $k++;
       }
-      if ($#license_expiry_warn_table > 0) {
-          $return_string = 'WARNING: License expiration '.join(';', @license_expiry_warn_table);
+      if (@license_expiry_warn_table) {
+          $return_string_errors .= sprintf(" WARNING[%s]", join(", ", @license_expiry_warn_table));
           $return_state = 'WARNING';
       }
-      if ($#license_expiry_crit_table > 0) {
-          $return_string = 'CRITICAL: License expiration '.join(';', @license_expiry_crit_table);
+      if (@license_expiry_crit_table{
+          $return_string_errors .= sprintf(" CRITICAL[%s]", join(", ", @license_expiry_crit_table));
           $return_state = 'CRITICAL';
       }
+      $return_string = $return_state . ": " . $curr_device . "  ";
+      $return_string .=  $return_string_errors;
    } else {
       $return_string = "UNKNOWN: device has no license checks available";
       $return_state = "UNKNOWN";
